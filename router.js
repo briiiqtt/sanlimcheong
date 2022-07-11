cce05f8f433ae45e87fd43b0870783150132f05b
briiiqtt <briiiqtt@gmail.com>
2022-07-11 09:28:54 +0900

    a

---
+++ router.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+
+app.use("/public", express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/test", (req, res) => {
+  res.sendFile(__dirname + "/test.html");
+});
+app.get("/main", (req, res) => {
+  res.sendFile(__dirname + "/main.html");
+});
+app.get("/patient-reg", (req, res) => {
+  res.sendFile(__dirname + "/patient-reg.html");
+});
+app.get("/patient-detail", (req, res) => {
+  res.sendFile(__dirname + "/patient-detail.html");
+});
+
+app.listen((port = 52522), () => {
+  console.log(`server started, port: ${port}`);
+});
